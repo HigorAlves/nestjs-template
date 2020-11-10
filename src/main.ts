a9fff1a9ef0582f9d058411fd06a9ec820e55504
Higor Alves <higorhaalves@gmail.com>
2020-11-10 19:25:40 -0300

    fix(heroku): constants

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
   app.enableCors();
-  Logger.log(`🚀 Server running on http://localhost:3000`, 'Bootstrap');
+  Logger.log(`🚀 Server running on http://localhost:5000`, 'Bootstrap');
 }
 
 bootstrap();
