5e09d8f964fd1aa99413f0bee98cf61fecdc694e
Higor Alves <higorhaalves@gmail.com>
2020-11-10 19:20:40 -0300

    fix(heroku): constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,29 +1,14 @@
 import * as Dotenv from 'dotenv';
-
-const dotenv = Dotenv.config().parsed;
+Dotenv.config();
 
 export const TYPEORM = {
-  authSource: dotenv.TYPEORM_AUTHSOURCE
-    ? dotenv.TYPEORM_AUTHSOURCE
-    : process.env.TYPEORM_AUTHSOURCE,
-  url: dotenv.TYPEORM_DATABASE_URL
-    ? dotenv.TYPEORM_DATABASE_URL
-    : process.env.TYPEORM_DATABASE_URL,
-  database: dotenv.TYPEORM_DATABASE
-    ? dotenv.TYPEORM_DATABASE
-    : process.env.TYPEORM_DATABASE,
-  synchronize: Boolean(
-    dotenv.TYPEORM_SYNCRONIZE
-      ? dotenv.TYPEORM_SYNCRONIZE
-      : process.env.TYPEORM_SYNCRONIZE,
-  ),
-  logging: Boolean(
-    dotenv.TYPEORM_LOGGING
-      ? dotenv.TYPEORM_SYNCRONIZE
-      : process.env.TYPEORM_LOGGING,
-  ),
+  authSource: process.env.TYPEORM_AUTHSOURCE,
+  url: process.env.TYPEORM_DATABASE_URL,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: Boolean(process.env.TYPEORM_SYNCRONIZE),
+  logging: Boolean(process.env.TYPEORM_LOGGING),
 };
 
 export const JWT = {
-  secret: dotenv.JWT_SECRET ? dotenv.JWT_SECRET : process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET,
 };
